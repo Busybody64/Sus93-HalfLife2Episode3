ac700ae2a299da2b99c8da0e77bd1c53bd39a926
Busybody64 <92069026+Busybody64@users.noreply.github.com>
2021-10-07 17:09:45 -0500

    Update and rename A Generic Program to HalfLife2Episode3.js
    A rather surprisingly realistic outcome, huh?

---
+++ HalfLife2Episode3.js
@@ -0,0 +1,1 @@
+$alert({msg:"Windows 93 is a 16-bit system. (Not really) As such, it cannot run 32-bit or 64-bit files, like Half-Life 2: Episode 3. Sorry about that!",center:!1,btnOk:"OK",title:"Compatability Error",animationOut:"none",icon:"https://i.imgur.com/zzz1KA1.png"})
